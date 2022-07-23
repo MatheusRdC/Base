956c7a22888c26848d850402a44a8b77dc816307
MatheusRdC <carvalhoribeiro09@gmail.com>
2022-07-23 18:29:43 -0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,6 @@ export function App() {
                 isOpen={isNewTransactionModalOpen}
                 onRequestClose={handleCloseNewTransactionModal}
             />
-
             <GlobalStyle />
         </>
     );
