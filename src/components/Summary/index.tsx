768165900c18fbbfa5ef9c3baf364d47b41e7f49
MatheusRdC <carvalhoribeiro09@gmail.com>
2022-08-02 19:45:18 -0300

    done

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,22 +8,6 @@ import { TransactionsContext } from "../../TransactionsContex";
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
 
-    const summary = transactions.reduce((acc, transaction) => {
-        if (transaction.type === 'deposit') {
-            acc.deposits += transaction.amount
-            acc.total += transaction.amount
-        } else {
-            acc.withdraws += transaction.amount
-            acc.total -= transaction.amount
-        }
-
-        return acc;
-    }, {
-        deposits: 0,
-        withdraws: 0,
-        total: 0,
-    });
-
     return (
         <Container>
 
@@ -33,9 +17,7 @@ export function Summary() {
                     <img src={incomeImg} alt="Entradas" />
                 </header>
                 <strong>
-                    {new Intl.NumberFormat('pt-BR',
-                        { style: 'currency', currency: 'BRL' })
-                        .format(summary.deposits)}
+                   
                 </strong>
             </div>
             <div>
@@ -44,9 +26,7 @@ export function Summary() {
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
                 <strong>
-                    {new Intl.NumberFormat('pt-BR',
-                        { style: 'currency', currency: 'BRL' })
-                        .format(summary.withdraws)}
+               
                 </strong>
             </div>
             <div>
@@ -55,9 +35,7 @@ export function Summary() {
                     <img src={totalImg} alt="Entradas" />
                 </header>
                 <strong>
-                    {new Intl.NumberFormat('pt-BR',
-                        { style: 'currency', currency: 'BRL' })
-                        .format(summary.total)}
+               
                 </strong>
             </div>
         </Container>
